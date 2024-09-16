faa45ea8f50396eab31d97bd52173e747fcb3b41
aproragadozo <aproragadozo@gmail.com>
2024-09-16 17:00:26 +0200

    serve static assets

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 let express = require('express');
 let app = express();
 
-app.get("/", (req, res) => {res.send("Hello World!")})
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {res.sendFile(__dirname + '/views/index.html')})
 
 
 
